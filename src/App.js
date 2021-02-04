d9b97cdac3a161f2ffa7024189c17598de56db41
luukimhoang2k <luukimhoangdz2022@gmail.com>
2021-02-05 05:13:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import anh2 from './anh2.jpg';
 import anh3 from './anh3.jpg';
 import anh4 from './anh4.jpg';
 import './App.css';
-
+const b={
+  title:[
+    {content:1,
+    image:anh1},
+    {content:2,
+    image:anh2},
+    {content:3,
+    image:anh3},
+    {content:4,
+      image:anh4},
+      {content:5,
+        image:logo}
+  ],
+  
+}
 function App() {
   return (
   <>
-        <div>
-          <h1>hello</h1>
+        <div className="container">
+          {b.title.map(i=> <div className="container-con"><p>{i.content}</p><img src={i.image}></img></div>)}
         </div>
   </>
   );
 }
+
 
 export default App;
